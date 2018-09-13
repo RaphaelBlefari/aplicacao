56a6d10f0eb7c1c7f0e13c0e48d232d1b4d8cb52
Raphael Blefari Goncalves <raphael.goncalves@viavarejo.com.br>
2018-09-13 17:25:58 -0300

    p

--- index.php
+++ index.php
@@ -13,9 +13,10 @@
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Aplicação</a>
-  <p><?php
-    echo ">>>> Env >>>".getenv('msgManutencao')."";
-    ?></p>
+  <?php
+    echo ">>>> Env >>>";
+    echo getenv('msgManutencao');
+    ?>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
