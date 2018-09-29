14703a8521214effcc84079201a1b771f10611be
Raphael <blefari_101@hotmail.com>
2018-09-29 17:25:31 -0300

    ...

--- index.php
+++ index.php
@@ -22,43 +22,13 @@
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 
 
 <div class="jumbotron">
-  <h1 class="display-4">Aplicção!</h1>
-  <?php
-  $x = 1;
-  $y = 2;
-
-    echo $x+$y;
-    ?>
-  <?php 
-    echo getenv('msgManutencao');              
-  ?>
+  <h1 class="display-4">Aplicação!</h1>
   <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 </div>
 
